440187b112c987908aede1b2df245f6f08157c54
kaio Guilherme <kaioguilherme444@gmail.com>
2024-10-25 10:46:31 -0400

    🚀 deploy(github): create builds automations

---
+++ config/setup_linux.py
@@ -0,0 +1,22 @@
+from cx_Freeze import setup, Executable
+
+# Opções de build
+build_exe_options = {
+    "packages": ["os", "requests"],
+    "include_files": [],
+    "build_exe": "build/linux",  # Define a pasta de saída
+}
+
+# Setup para gerar o binário no Linux
+setup(
+    name="Embracedata-Downloader",
+    version="0.1.0",
+    description="Ferramenta automatizada para download de dados da plataforma Embracedata do INPE",
+    options={"build_exe": build_exe_options},
+    executables=[
+        Executable(
+            "src/Embracedata_Downloader.py",  # Caminho para o arquivo principal
+            target_name="Embracedata-Downloader",  # Nome do binário
+        )
+    ],
+)
